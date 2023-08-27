37997b8f4a1416bd879a2d44ba644c6e9c3564b0
Jonathan David <jhernadezcorrea@gmail.com>
2023-08-26 20:39:29 -0500

    fixing errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,12 +56,12 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     origin:'top',
     distance: '60px',
-    duration: 2500,
-    delay: 400,  
+    duration: 100,
+    delay: 100,  
 })
 
 sr.reveal(`.profile__border`)
-sr.reveal(`.profile__name`,{delay:500})
+sr.reveal(`.profile__name`,{delay:100})
 sr.reveal(`.profile__profession`,{delay:600})
 sr.reveal(`.profile__social`,{delay:700})
 sr.reveal(`.profile__info-group`,{interval:100, delay:700})
